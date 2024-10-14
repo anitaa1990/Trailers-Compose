653b8964a53c96973136df9458bd8894bc432bbb
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-14 15:43:36 +0400

    Added logic to fetch & store movie detail & update the UI

--- app/src/main/java/com/an/trailers_compose/ui/detail/movie/MovieDetailScreen.kt
+++ app/src/main/java/com/an/trailers_compose/ui/detail/movie/MovieDetailScreen.kt
@@ -1,7 +1,12 @@
 package com.an.trailers_compose.ui.detail.movie
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
-fun MovieDetailScreen() {
+fun MovieDetailScreen(viewModel: MovieDetailViewModel) {
+    val movieUiState = viewModel.movieUiState.collectAsStateWithLifecycle(
+        lifecycleOwner = LocalLifecycleOwner.current
+    )
 }
