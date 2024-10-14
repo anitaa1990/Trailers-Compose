303ccf2ea186d406bda916c2eb8f88294142c32c
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-14 13:21:49 +0400

    Added Loading screen & Empty screen when loading movies for the first time

---
+++ app/src/main/java/com/an/trailers_compose/ui/component/EmptyScreen.kt
@@ -0,0 +1,56 @@
+package com.an.trailers_compose.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.an.trailers_compose.R
+
+@Composable
+fun EmptyScreen(
+    errorMessage: String,
+    onRefreshClicked:() -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            modifier = Modifier.size(64.dp),
+            imageVector = Icons.Rounded.Warning, contentDescription = ""
+        )
+
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = errorMessage,
+            textAlign = TextAlign.Center,
+        )
+
+        Button(
+            onClick = { onRefreshClicked() },
+            content = {
+                Text(text = stringResource(id = R.string.refresh))
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White,
+            )
+        )
+    }
+}
