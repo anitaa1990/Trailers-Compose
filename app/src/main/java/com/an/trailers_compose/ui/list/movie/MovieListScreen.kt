134159ec04a4902375379640babfda9b4e944855
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-18 19:49:13 +0530

    Removed Top app bar and bottom bar

--- app/src/main/java/com/an/trailers_compose/ui/list/movie/MovieListScreen.kt
+++ app/src/main/java/com/an/trailers_compose/ui/list/movie/MovieListScreen.kt
@@ -8,22 +8,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import com.an.trailers_compose.R
 import com.an.trailers_compose.data.local.entity.MovieEntity
 import com.an.trailers_compose.ui.component.CircleRevealPager
 import com.an.trailers_compose.ui.component.EmptyScreen
 import com.an.trailers_compose.ui.component.LoadingItem
-import com.an.trailers_compose.ui.component.ProvideAppBarTitle
-import com.an.trailers_compose.ui.component.TopBarTitle
 
 @Composable
 fun MovieListScreen(
     movies: LazyPagingItems<MovieEntity>,
     onItemClicked: (remoteId: Long) -> Unit
 ) {
-    // Toolbar title
-    ProvideAppBarTitle { TopBarTitle(text = R.string.title_movies) }
-
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -32,7 +26,7 @@ fun MovieListScreen(
         // Different load states – Loading, Empty State, Pager list state
         val loadState = movies.loadState.mediator
         when (loadState?.refresh) {
-            LoadState.Loading -> {
+            is LoadState.Loading -> {
                 LoadingItem()
             }
             is LoadState.Error -> {
@@ -41,7 +35,6 @@ fun MovieListScreen(
                     movies.refresh()
                 }
             }
-
             else -> {
                 // Movie List
                 CircleRevealPager(movies)
