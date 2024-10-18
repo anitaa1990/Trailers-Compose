a406fa8b510ebd1d4275333b05c6c6dce91ce620
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-18 19:42:51 +0530

    Modified list screen UI for movies

--- app/src/main/java/com/an/trailers_compose/ui/list/movie/MovieListScreen.kt
+++ app/src/main/java/com/an/trailers_compose/ui/list/movie/MovieListScreen.kt
@@ -1,33 +1,16 @@
 package com.an.trailers_compose.ui.list.movie
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
-import coil.compose.AsyncImage
-import com.an.trailers_compose.AppConstants.IMAGE_URL
 import com.an.trailers_compose.R
 import com.an.trailers_compose.data.local.entity.MovieEntity
-import com.an.trailers_compose.ui.component.CustomSearchBar
+import com.an.trailers_compose.ui.component.CircleRevealPager
 import com.an.trailers_compose.ui.component.EmptyScreen
 import com.an.trailers_compose.ui.component.LoadingItem
 import com.an.trailers_compose.ui.component.ProvideAppBarTitle
@@ -46,9 +29,6 @@ fun MovieListScreen(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Search Bar
-        CustomSearchBar()
-
         // Different load states – Loading, Empty State, Pager list state
         val loadState = movies.loadState.mediator
         when (loadState?.refresh) {
@@ -64,68 +44,8 @@ fun MovieListScreen(
 
             else -> {
                 // Movie List
-                val pagerState = rememberPagerState(
-                    pageCount = { movies.itemCount }
-                )
-                HorizontalPager(
-                    state = pagerState,
-                    pageSize = PageSize.Fill
-                ) { index ->
-                    movies[index]?.let {
-                        MovieListItem(
-                            movie = it,
-                            onItemClicked = {
-                                onItemClicked(it.remoteId)
-                            }
-                        )
-                    }
-                }
+                CircleRevealPager(movies)
             }
         }
-    }
-}
-
-@Composable
-fun MovieListItem(
-    movie: MovieEntity,
-    onItemClicked: () -> Unit
-) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .clickable { onItemClicked() },
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Card(
-            modifier = Modifier.fillMaxSize(0.85f),
-            shape = RoundedCornerShape(20.dp),
-            elevation = CardDefaults.cardElevation(20.dp)
-        ) {
-            AsyncImage(
-                model = String.format(IMAGE_URL, movie.posterPath),
-                modifier = Modifier.fillMaxSize(),
-                contentDescription = "",
-                contentScale = ContentScale.Crop
-            )
-        }
-        Text(
-            modifier = Modifier
-                .fillMaxWidth(0.9f)
-                .padding(top = 20.dp, bottom = 2.dp),
-            text = movie.title,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.primary
-        )
-        Text(
-            modifier = Modifier.fillMaxWidth(0.6f),
-            text = movie.tagline ?: movie.overview,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodySmall,
-            color = MaterialTheme.colorScheme.secondary
-        )
     }
 }
