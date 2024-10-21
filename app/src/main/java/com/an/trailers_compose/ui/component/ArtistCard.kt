68cfb9977048d9d737d8137c4695f3404bc46036
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-21 17:51:06 +0530

    Added placeholder drawable if cast/crew image is not found

--- app/src/main/java/com/an/trailers_compose/ui/component/ArtistCard.kt
+++ app/src/main/java/com/an/trailers_compose/ui/component/ArtistCard.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -23,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.transform.CircleCropTransformation
+import com.an.trailers_compose.R
 import com.an.trailers_compose.ui.model.Artist
 
 @Composable
@@ -73,12 +76,15 @@ private fun ArtistListItem(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(artist.imageUrl)
                 .transformations(CircleCropTransformation())
+                .placeholder(R.drawable.ic_placeholder_profile)
+                .error(R.drawable.ic_placeholder_profile)
                 .build(),
             contentDescription = "",
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .width(90.dp)
                 .height(90.dp)
+                .clip(CircleShape)
         )
         Text(
             text = artist.name,
