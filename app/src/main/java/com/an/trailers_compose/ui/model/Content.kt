73f6d63218e843881c1e38924f9a67f11a0ae809
Anitaa Murthy <murthyanitaa@gmail.com>
2024-10-24 01:03:03 +0530

    Added TvViewModel class

--- app/src/main/java/com/an/trailers_compose/ui/model/Content.kt
+++ app/src/main/java/com/an/trailers_compose/ui/model/Content.kt
@@ -5,8 +5,10 @@ import com.an.trailers_compose.AppConstants
 import com.an.trailers_compose.AppConstants.MOVIE_STATUS_RELEASED
 import com.an.trailers_compose.AppConstants.genreColors
 import com.an.trailers_compose.data.local.entity.MovieEntity
+import com.an.trailers_compose.data.local.entity.TvEntity
 import com.an.trailers_compose.data.remote.model.Genre
 import com.an.trailers_compose.data.remote.model.Video
+import com.an.trailers_compose.utils.getFormattedDate
 import java.util.*
 
 data class Content(
@@ -32,15 +34,15 @@ data class SimilarContent(
 
 fun MovieEntity.toSimilarContent() = SimilarContent(
     id = this.remoteId,
-    posterUrl = getPosterUrl()
+    posterUrl = getPosterUrl(this.posterPath)
 )
 
 fun MovieEntity.toContent() = Content(
     id = this.remoteId,
     title = this.title,
     description = this.overview,
-    posterUrl = getPosterUrl(),
-    backdropUrl = getBackdropUrl(),
+    posterUrl = getPosterUrl(this.posterPath),
+    backdropUrl = getBackdropUrl(this.backdropPath),
     genres = this.genres ?: emptyList(),
     trailers = this.videoApiResponse?.videos ?: emptyList(),
     status = this.status ?: "",
@@ -51,8 +53,29 @@ fun MovieEntity.toContent() = Content(
     genreBgColor = genreColors[Random().nextInt(genreColors.size)]
 )
 
-private fun MovieEntity.getDuration() = if (MOVIE_STATUS_RELEASED == status)
-    runtime.toString() else releaseDate
+fun TvEntity.toSimilarContent() = SimilarContent(
+    id = this.remoteId,
+    posterUrl = getPosterUrl(this.posterPath)
+)
 
-private fun MovieEntity.getPosterUrl() = String.format(AppConstants.IMAGE_URL, this.posterPath)
-private fun MovieEntity.getBackdropUrl() = String.format(AppConstants.IMAGE_URL, this.backdropPath)
+fun TvEntity.toContent() = Content(
+    id = this.remoteId,
+    title = this.title,
+    description = this.overview,
+    posterUrl = getPosterUrl(this.posterPath),
+    backdropUrl = getBackdropUrl(this.backdropPath),
+    genres = this.genres ?: emptyList(),
+    trailers = this.videoApiResponse?.videos ?: emptyList(),
+    status = this.status ?: "",
+    durationInMins = this.releaseDate.getFormattedDate(),
+    cast = this.credits?.cast?.map { it.toArtist() } ?: emptyList(),
+    crew = this.credits?.crew?.map { it.toArtist() } ?: emptyList(),
+    similarMovies = this.similarTvApiResponse?.results?.map { it.toSimilarContent() } ?: emptyList(),
+    genreBgColor = genreColors[Random().nextInt(genreColors.size)]
+)
+
+private fun MovieEntity.getDuration() = if (MOVIE_STATUS_RELEASED == status)
+    String.format("%s mins", runtime.toString()) else releaseDate.getFormattedDate()
+
+private fun getPosterUrl(posterPath: String?) = String.format(AppConstants.IMAGE_URL, posterPath)
+private fun getBackdropUrl(backdropPath: String?) = String.format(AppConstants.IMAGE_URL, backdropPath)
